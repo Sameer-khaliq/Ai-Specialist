83f93a21f432ebe97064f9ea9a7330736134ab6e
Sameer-khaliq <sameerkhaliq58@gmail.com>
2026-06-05 10:58:03 +0500

    Backend testing with fastapi

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def main():
     print("Hello from ai-specialist!")
 
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"status": "Day 1 complete", "stack": "ready"}
 if __name__ == "__main__":
     main()
