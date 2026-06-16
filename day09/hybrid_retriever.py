3a160094273ba506be3e026ab3105d59afbf8c43
Sameer-khaliq <sameerkhaliq58@gmail.com>
2026-06-16 15:07:59 +0500

    some changes

--- day09/hybrid_retriever.py
+++ day09/hybrid_retriever.py
@@ -2,7 +2,6 @@ import os
 from pypdf import PdfReader
 from rank_bm25 import BM25Okapi
 import chromadb
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
 
 load_dotenv()
