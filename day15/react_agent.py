c2cfa850689aa32d357c822fc89382684b14fc26
Sameer-khaliq <sameerkhaliq58@gmail.com>
2026-06-23 16:03:03 +0500

    updated version

--- day15/react_agent.py
+++ day15/react_agent.py
@@ -9,7 +9,7 @@ from langchain_classic.agents import AgentExecutor, create_react_agent
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
-    model = "gemini-2.5-flash-lite",
+    model = "gemini-2.5-flash",
     google_api_key = os.getenv("GEMINI_API_KEY")
 )
 
