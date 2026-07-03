b6ab7c56b1c1674ac396fd3e124c2c81e0ca482e
Sameer-khaliq <sameerkhaliq58@gmail.com>
2026-07-03 12:47:37 +0500

    Load environment variables using dotenv in runners.py

--- day26/runners.py
+++ day26/runners.py
@@ -4,7 +4,8 @@ import ollama
 from groq import Groq
 from google import genai
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 groq_client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 gemini_client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
